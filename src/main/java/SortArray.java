159da1a69a051a3b84b1e7c8d12fd77f3baaeef0
IvanLihoy <Lihoy92ivan@gmail.com>
2017-06-01 23:44:06 +0300

    sort an array

--- src/main/java/SortArray.java
+++ src/main/java/SortArray.java
@@ -4,24 +4,20 @@ import java.util.List;
 
 
 public class SortArray {
+    
     public static void main(String[] args) {
        sortArrayNumbers();
-
-
     }
 
     private static void sortArrayNumbers() {
         Integer[] arr = new Integer[]{9, 2, 7, 4, 5, 6, 3, 8, 1, 9, 22, 34, 84};
 
-//        Arrays.sort(arr, Collections.reverseOrder());
-//        System.out.println(Arrays.toString(arr));
-
-//        for(Integer i : arr){
-//            System.out.println(i.intValue());
-//        }
-
         List<Integer> list = Arrays.asList(arr);
         Collections.sort(list, Collections.<Integer>reverseOrder());
         System.out.println(list);
+
+//        Arrays.sort(arr, Collections.reverseOrder());
+//        System.out.println(Arrays.toString(arr));
+
     }
 }
