538d6c6a82c64f030074c782b4c209aa8a8e9f04
IvanLihoy <Lihoy92ivan@gmail.com>
2017-06-05 23:34:44 +0300

    validation email

---
+++ src/main/java/ValidateEmail.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class ValidateEmail {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(isValidEmailAddress(scanner.next()));
+    }
+
+    private static boolean isValidEmailAddress(String email) {
+        String regex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern p = java.util.regex.Pattern.compile(regex);
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+}
