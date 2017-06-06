4ea1d0eebd077b9fc2b04fd7cbfa5252f16b64af
IvanLihoy <lihoy92ivan@gmail.com>
2017-06-06 21:00:44 +0300

    course

--- src/main/java/ValidateEmail.java
+++ src/main/java/ValidateEmail.java
@@ -5,14 +5,26 @@ import java.util.regex.Pattern;
 
 public class ValidateEmail {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(isValidEmailAddress(scanner.next()));
+        //System.out.println(isNumberBetween("5000"));
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(isValidEmailAddress(scanner.next()));
+        System.out.println(isNumberBetween("5000.22"));
+
     }
 
-    private static boolean isValidEmailAddress(String email) {
-        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(email);
-        return m.matches();
+
+//    private static boolean isValidEmailAddress(String email) {
+//        String regex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+//        Pattern p = Pattern.compile(regex);
+//        Matcher m = p.matcher(email);
+//        return m.matches();
+//
+//    }
+    public static boolean isNumberBetween(String input){
+        input = "5000,1000,5001";
+
+        return Pattern.matches("^(?:Numbers: )?(?:(?:[2-9][0-9]{3}|10000)(?:\\.\\d\\d)?(?:,|$))+$", input);
+
+
     }
 }
