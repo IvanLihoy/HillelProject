779a01beb7a27df21154e011c0b8ea400e802b0c
Ivan <Lihoy92ivan@gmail.com>
2017-06-07 17:17:34 +0300

    gmail

--- src/main/java/CheckGMail.java
+++ src/main/java/CheckGMail.java
@@ -7,14 +7,10 @@ public class CheckGMail {
     public static void main(String[] args) {
         String input = "email1@gmail.com.,email2@gmail.com,email3@gmail.com,email4@gmail.com";
         System.out.println(isAGMailAddress(input));
-
     }
+
     private static boolean isAGMailAddress(String input){
         //return Pattern.matches("^(([a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[a-zA-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@(?:gmail\\.com))?(?:,|$))+$", input);
-        //return Pattern.matches("^((([a-zA-Z0-9_!#$%&'*+/=?`{|}~^-])+(?:\\.\\1)*@(?:gmail\\.com))?(?:,|$))+$", input);
-        return Pattern.matches("^(?:.+@)?(?:gmail\\.com)?(?:,|$)+$", input);
-
-
-        //return Pattern.matches("^(?:Numbers: )?(?:(?:[2-9][0-9]{3}|10000)(?:\\.\\d\\d)?(?:,|$))+$", input);
+        return Pattern.matches("^((([a-zA-Z0-9_!#$%&'*+/=?`{|}~^-])+(?:\\.\\1)*@(?:gmail\\.com))?(?:,|$))+$", input);
     }
 }
