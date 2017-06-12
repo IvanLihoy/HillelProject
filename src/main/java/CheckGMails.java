1a7756b87eb7b7a9f35e35ee9878076f3612e558
Ivan <Lihoy92ivan@gmail.com>
2017-06-12 14:27:44 +0300

    check gmail

---
+++ src/main/java/CheckGMails.java
@@ -0,0 +1,14 @@
+import java.util.regex.Pattern;
+
+
+
+public class CheckGMails {
+    public static void main(String[] args) {
+        String input = "email1@gmail.com,email2@gmail.com,email3@gmail.com";
+        System.out.println(isAGMailAddress(input));
+    }
+
+    private static boolean isAGMailAddress(String input){
+        return Pattern.matches("(([^@]*)@(?:gmail\\.com)?(?:,|$))+$", input);
+    }
+}
