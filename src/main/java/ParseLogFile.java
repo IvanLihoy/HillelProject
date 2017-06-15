05b5a08f31ab01c37b647f8ee977e7929949083d
Ivan <Lihoy92ivan@gmail.com>
2017-06-15 16:37:28 +0300

    parse log file

--- src/main/java/ParseLogFile.java
+++ src/main/java/ParseLogFile.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,38 +6,27 @@ import java.util.regex.Pattern;
 
 public class ParseLogFile {
     public static void main(String[] args) throws FileNotFoundException {
-        //String input = "email1@gmail.com,email2@gmail.com,email3@gmail.com,email3@yah.oo.com";
-        //System.out.println(isAGMailAddress(input));
-        parseFile();
-
+        try{
+            String filePath = "C:\\Users\\Ivan\\IdeaProjects\\HillelProject\\src\\main\\java\\file.txt";
+            parseFile(filePath);
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
     }
 
-    private static boolean isAGMailAddress(String input) {
-        //return Pattern.matches("(([^@]*)@(?:gmail\\.com)?(?:,|$))+$", input);
-
-        return Pattern.matches("^(?i)(?:(?:[^@]+((@(gmail|yahoo|yandex)\\.com)))?(?:,|$))+$", input);
-    }
-
-
-    private static void parseFile() throws FileNotFoundException {
+    private static void parseFile(String filePath) throws FileNotFoundException {
         String line;
-        String filePath = "/home/hillel/IdeaProjects/HillelProject/src/main/java/file.txt";
-
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             while ((line = br.readLine()) != null) {
-//               Pattern.matcher(".+",line);
-//                System.out.println(line);
-                String regex = ".+";
+                String regex = "(?:^(.*?)Activity:)|((?:\\[(?:Login Username:|Data Object:|Records:|Records:|User Action Status:|Labels:|Service type:|Mapping Ids:|URI:)(.*?)]))";
                 Pattern p = Pattern.compile(regex);
                 Matcher m = p.matcher(line);
                 while (m.find())
                     System.out.println(m.group());
-
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
